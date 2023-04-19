b872ea6a43c0c79cca7f5de641c039150b1db054
shrddha <shrddha@lagran.ingit config --global user.email shrddha@lagran.in>
2023-04-19 10:42:47 +0530

    comment here

--- app/views.py
+++ app/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
-
 def homePage(request):
-    return render(request,"index.html")
+    data={
+        'title':'Home Page',
+        'bdata':'Welcome to mysite',
+        'clist':['php','c','c++','Django'],
+        'student_details':[
+            {'Name':'Shrddha','Phone':8855949556},
+            {'Name':'Nishu','Phone':7588425404}
+        ]
+    }
+    return render(request,"index.html",data)
 
 def aboutUs(request):
     return HttpResponse("<h1>Welcome to Lagran Family</h1>")
